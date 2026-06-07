f1f81ee38dc1f24e0552841098087c6821fd48f8
Gionata2266 <gionata2266@hotmail.com>
2026-06-07 14:29:51 -0300

    feat: adicionando tela head ou inicial

--- profile/src/App.jsx
+++ profile/src/App.jsx
@@ -3,7 +3,21 @@ import './App.css'
 function App() {
   return (
     <>
-      <section className="caixa"></section>
+      <section className="caixa">
+        <div className='header'>
+          <div className='nome'><h2>Gionata Disconzi</h2></div>
+
+          <div className='header2'>
+            <p>Home</p>
+            <p>Sobre</p>
+            <p>Tecnologias</p>
+            <p>Progetos</p>
+            <p>Contato</p>
+            <p>?@?</p>
+          </div>
+
+        </div>
+      </section>
     </>
   )
 }
